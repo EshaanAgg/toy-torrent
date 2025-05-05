bb93347e6bfd66450ece235038d457226587a622
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2025-05-06 01:46:21 +0530

    add support for piece hashes

--- app/cmd/info.go
+++ app/cmd/info.go
@@ -8,6 +8,35 @@ import (
 	"github.com/EshaanAgg/toy-bittorrent/app/utils"
 )
 
+// printPieceHashes prints the piece hashes from the pieces byte array.
+// Each piece hash is 20 bytes long, so the length of the pieces byte array
+// should be a multiple of 20.
+func printPieceHashes(pieces []byte) {
+	if len(pieces)%20 != 0 {
+		fmt.Printf("pieces length is not a multiple of 20")
+		return
+	}
+
+	fmt.Println("Piece Hashes:")
+	for i := 0; i < len(pieces); i += 20 {
+		pieceHash := pieces[i : i+20]
+		fmt.Printf("%x\n", pieceHash)
+	}
+}
+
+func printInfoRelatedFields(infoDict *bencode.BencodeDictionary) {
+	infoHash, err := utils.SHA1Hash(infoDict.Encode())
+	if err != nil {
+		fmt.Printf("error hashing the info dictionary: %v", err)
+		return
+	}
+
+	fmt.Printf("Info Hash: %s\n", infoHash)
+	fmt.Printf("Piece Length: %d\n", infoDict.Map["piece length"].GetInteger().Value)
+	pieces := infoDict.Map["pieces"].GetString().Value
+	printPieceHashes(pieces)
+}
+
 func HandleInfo(args []string) {
 	// Validate the number of arguments passed to the info command
 	if len(args) == 0 {
@@ -45,10 +74,5 @@ func HandleInfo(args []string) {
 	fileSize := infoDict.Map["length"].GetInteger().Value
 	fmt.Printf("Length: %d\n", fileSize)
 
-	infoHash, err := utils.SHA1Hash(infoDict.Encode())
-	if err != nil {
-		fmt.Printf("error hashing the info dictionary: %v", err)
-		return
-	}
-	fmt.Printf("Info Hash: %s\n", infoHash)
+	printInfoRelatedFields(infoDict)
 }
